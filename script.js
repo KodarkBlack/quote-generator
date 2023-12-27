155a8750b9b9152e9c28c17ccfb6dba846a918fb
Olufisayo Israel Tomide <148365041+JthefrontendBoy@users.noreply.github.com>
2023-12-27 22:14:34 +0100

    home

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ quoteBtn = document.querySelector("button")
 soundBtn = document.querySelector(".sound")
 copyBtn = document.querySelector(".copy")
 twitterBtn = document.querySelector(".twitter")
-
 // Random quote function
 
 randomQuote =  () => {
     quoteBtn.classList.add("loading")
     quoteBtn.innerText = "Loading Quote....";
-    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+    
+    fetch("https://api.quotable.io/random")
+
+    .then(res => res.json())
+
+    .then(result => {
         console.log(result)
         quoteText.innerText = result.content;
         authorName.innerText = result.author;
@@ -36,3 +40,5 @@ twitterBtn.addEventListener("click", ()=> {
 
 
 quoteBtn.addEventListener("click", randomQuote)
+
+
