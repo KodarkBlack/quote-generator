889674ca56f4c8dfb573b3ece03e37c84d8380ff
Olufisayo Israel Tomide <ibukunolufisayo8888@gmail.com>
2024-01-06 22:30:29 +0100

    lordicon

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 import {getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,18 +14,25 @@ const firebaseConfig = {
   storageBucket: "miderandomquote.appspot.com",
   messagingSenderId: "691429738075",
   appId: "1:691429738075:web:a11ee915c8b764816ee17e",
-  measurementId: "G-B7BQGG62D5"
+  // measurementId: "G-B7BQGG62D5"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // get the auth instance 
 const auth = getAuth(app)
+let signBtn = document.getElementById("signUp");
+
 
 function handleSignUp(event){
   event.preventDefault();
+
+
+  setTimeout(() =>{
+    signBtn.textContent = "Proceeding..."
+  }, 1000)
 
   // the the user input
 
@@ -37,7 +44,18 @@ function handleSignUp(event){
   createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     const user = userCredential.user;
+    setTimeout(()=>{
+      signBtn.innerText = "Signed Up Successfully"
+    },1000)
     console.log('User signed up:', user)
+
+    // Clear the form feed whe user is logged in
+
+    email.value = "";
+    password.value = "";
+
+    return ('User signed up:', user)
+
   })
   .catch((error) => {
     const errorCode = error.code;
